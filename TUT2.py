ac4d61884705b5d1afe0dccfd4af6e7a23e76da1
kushal-a <98091396+kushal-a@users.noreply.github.com>
2022-01-27 18:48:45 +0530

    Delete TUT2.py

--- TUT2.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-beehive_data = np.loadtxt('Beehive_data.csv', delimiter=',')
-distances =10*10**((beehive_data[:,0]-4.83)/5+beehive_data[:,1]/2) # since we take unit distance as 10 parsec
-meanDistance=sum(distances*beehive_data[:,2])/sum(beehive_data[:,2])
-print (meanDistance)
